b07d279e8ea775b08695c8d29f03596d98d6691f
gardoroman <gardoroman@gmail.com>
2016-09-20 22:56:07 -0500

    User can register, log-in, and log-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'sessions#new'
 
   get '/lead_uploads/new' => 'lead_uploads#new'
   post '/lead_uploads' => 'lead_uploads#create'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   get '/sessions/new'=> 'sessions#new', :as => 'new_session'
   get '/sessions' => 'sessions#new'
   delete '/sessions' => 'sessions#delete'
+
 
   resources :users
 
